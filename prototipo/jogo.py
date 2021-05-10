15a4b8e4a3ecdde7dc34f3293428a616686df330
yokiyoki87 <someyokisoba@gmail.com>
2021-05-10 13:25:07 -0300

    gerenciamento de jogos salvos

--- prototipo/jogo.py
+++ prototipo/jogo.py
@@ -27,14 +27,14 @@ class Menu_Principal(Tela_Menu):  # QUASE QUE UMA INSTANCIA DA CLASSE TELA_MENU
         b7 = Botao(250, 225, 100, 50, (220, 0, 220), (160, 0, 160), "Fase 7", 5)
         b8 = Botao(450, 225, 100, 50, (220, 110, 220), (160, 80, 160), "Fase 8", 5)
         b9 = Botao(650, 225, 100, 50, (220, 220, 220), (160, 160, 160), "Fase 9", 5)
-        botaojogar = Botao(375, 350, 250, 50, (30, 220, 30), (30, 160, 30), "Novo Jogo", 5)
-        botaocontinuar = Botao(375, 405, 250, 50, (220, 220, 30), (160, 160, 30), "Carregar Jogo", 5)
-        botaoconfig = Botao(375, 460, 250, 50, (0, 220, 180), (0, 160, 110), "Configurações", 5)
+        botaojogar = Botao(375, 350, 250, 50, (30, 220, 30), (30, 160, 30), "Jogar", 5)
+        botaocontinuar = Botao(375, 405, 250, 50, (220, 220, 30), (160, 160, 30), "-", 5)
+        botaoconfig = Botao(375, 460, 250, 50, (0, 220, 180), (0, 160, 130), "Configurações", 5)
         botaosair = Botao(375, 515, 250, 50, (220, 30, 30), (160, 30, 30), "Sair", 5)
         cormenu = misturacor(psicodelico(0), [255, 255, 255], 1, 5)
         listabotoes = [botaosair, botaojogar, botaocontinuar, botaonivel_1, botaonivel_2,
                           botaonivel_3, botaoconfig,botaocontinuar,b4,b5,b6,b7,b8,b9]
-        listatelas = [True,False,[Novo_Jogo,[superficie]],[Carregar_Jogo,[superficie]],[Tela_De_Jogo,[superficie,"fase1",'6']]
+        listatelas = [True,False,[Carregar_Jogo,[superficie]],True,[Tela_De_Jogo,[superficie,"fase1",'6']]
                 ,[Tela_De_Jogo,[superficie,"fase2",'6']],[Tela_De_Jogo,[superficie,"fase3",'6']],True,True,True,True,True,True,True,True]
         super().__init__(listabotoes, cormenu, superficie,listatelas)
         self.__contador_menu = 0
@@ -60,26 +60,29 @@ class Carregar_Jogo(Tela_Menu):
         try:
             with open("saves.json","r") as saves:
                 slots = json.load(saves)
-                print("yes",slots)
         except:
             with open("saves.json","w") as saves:
-                slots = json.dumps({'0':["Slot Vazio","fase1"],'1':["Slot Vazio","fase1"],'2':["Slot Vazio","fase1"]
-                          ,'3':["Slot Vazio","fase1"],'4':["Slot Vazio","fase1"]},saves)
-                print("no",slots)
-        slot1 = slots['0']
-        slot2 = slots['1']
-        slot3 = slots['2']
-        slot4 = slots['3']
-        slot5 = slots['4']
-        b1 = Botao(250, 150, 200, 50, (220, 0, 110), (100, 80, 20), slot1[0], 5)
-        b2 = Botao(250, 210, 200, 50, (220, 0, 110), (100, 80, 20), slot2[0], 5)
-        b3 = Botao(250, 270, 200, 50, (220, 0, 110), (100, 80, 20), slot3[0], 5)
-        b4 = Botao(250, 330, 200, 50, (220, 0, 110), (100, 80, 20), slot4[0], 5)
-        b5 = Botao(250, 390, 200, 50, (220, 0, 110), (100, 80, 20), slot5[0], 5)
-        sair = Botao(200, 470, 200, 50, (220, 0, 110), (100, 80, 20), "Sair", 5)
-        listabotoes = [b1,b2,b3,b4,b5]
-        listatelas = [True,[Tela_De_Jogo,[superficie,slot1[1],'0']],[Tela_De_Jogo,[superficie,slot2[1],'1']]
-        ,[Tela_De_Jogo,[superficie,slot3[1],'2']],[Tela_De_Jogo,[superficie,slot4[1],'3'],],[Tela_De_Jogo,[superficie,slot5[1],'4']]]
+                slots = json.dumps({'0':["Novo Jogo","fase1"],'1':["Novo Jogo","fase1"],'2':["Novo Jogo","fase1"]
+                          ,'3':["Novo Jogo","fase1"],'4':["Novo Jogo","fase1"]},saves)
+        
+        encaixe = [slots[str(i)] for i in range(5)]
+        deletar_encaixe = ["Deletar" if slots[str(i)][0] != "Novo Jogo" else False for i in range(5)]
+
+        botoes_encaixe = [Botao(150, 200+60*i, 500, 50, (60, 220, 20), (20, 100, 0), encaixe[i][0], 5) if deletar_encaixe[i] == "Deletar"
+                        else Botao(150, 200+60*i, 500, 50, (160, 160, 160), (120, 120, 120), encaixe[i][0], 5) for i in range(5)]
+        sair = Botao(75, 520, 200, 50, (220, 20, 60), (100, 0, 20), "Sair", 5)
+        botoes_deletar = [Botao(700, 205+60*i, 100, 40, (220, 20, 60), (100, 0, 20), "Deletar", 5) if deletar_encaixe[i] == "Deletar" 
+                        else Botao(-1000, -1000, 50, 50, (0,0,0), (0,0,0), "", 5) for i in range(5)]
+        texto = Botao(100, 50, 400, 100, (200, 200, 200), (200, 200, 200), "Escolha de Jogo Salvo", 5)
+        
+        listabotoes = botoes_encaixe + botoes_deletar + [sair] + [texto]
+
+        listatelas = [True] + [[Tela_De_Jogo,
+                               [superficie,encaixe[i][1],str(i)]] 
+                               for i in range(5)] + [[Deletar_Save,
+                                                     [superficie,str(i)]] 
+                                                     for i in range(5)] + [[Menu_Principal,[superficie]]] + [True]
+
         cormenu = misturacor(psicodelico(0), [255, 255, 255], 1, 5)
         super().__init__(listabotoes,cormenu,superficie,listatelas)
         self.__contador_menu = 0
@@ -96,34 +99,34 @@ class Carregar_Jogo(Tela_Menu):
         return super().atualizar()
 
 
-class Novo_Jogo(Carregar_Jogo):
-    def __init__(self,superficie):
-        try:
-            with open("saves.json","r") as saves:
-                slots = json.load(saves)
-                print("yes",slots)
-        except:
-            with open("saves.json","w") as saves:
-                slots = {'0':["Slot Vazio","fase1"],'1':["Slot Vazio","fase1"],'2':["Slot Vazio","fase1"]
-                          ,'3':["Slot Vazio","fase1"],'4':["Slot Vazio","fase1"]}
-                json.dump(slots,saves)
-        slot1 = slots['0']
-        slot2 = slots['1']
-        slot3 = slots['2']
-        slot4 = slots['3']
-        slot5 = slots['4']
-        b1 = Botao(250, 150, 200, 50, (220, 0, 110), (100, 80, 20), slot1[0], 5)
-        b2 = Botao(250, 210, 200, 50, (220, 0, 110), (100, 80, 20), slot2[0], 5)
-        b3 = Botao(250, 270, 200, 50, (220, 0, 110), (100, 80, 20), slot3[0], 5)
-        b4 = Botao(250, 330, 200, 50, (220, 0, 110), (100, 80, 20), slot4[0], 5)
-        b5 = Botao(250, 390, 200, 50, (220, 0, 110), (100, 80, 20), slot5[0], 5)
-        sair = Botao(200, 470, 200, 50, (220, 0, 110), (100, 80, 20), "Sair", 5)
-        listabotoes = [b1,b2,b3,b4,b5]
-        listatelas = [True,[Tela_De_Jogo,[superficie,'fase1','0']],[Tela_De_Jogo,[superficie,'fase1','1']]
-        ,[Tela_De_Jogo,[superficie,'fase1','2']],[Tela_De_Jogo,[superficie,'fase1','3'],],[Tela_De_Jogo,[superficie,'fase1','4']]]
+class Deletar_Save(Tela_Menu):
+    def __init__(self,superficie,save):
+        self.__save = save
+        texto = Botao(200, 170, 600, 150, (200, 200, 200), (200, 200, 200), "Quer mesmo deletar esse jogo salvo?", 5)
+        deletar = Botao(150, 350, 320, 50, (220, 20, 60), (100, 0, 20), "Deletar", 5)
+        cancelar = Botao(530, 350, 320, 50, (60, 220, 20), (20, 100, 0), "Cancelar", 5)
+        listabotoes = [deletar,cancelar,texto]
+        listatelas = [True,[Carregar_Jogo,[superficie]],[Carregar_Jogo,[superficie]],True]
         cormenu = misturacor(psicodelico(0), [255, 255, 255], 1, 5)
-        super().__init__(superficie)
+        super().__init__(listabotoes,cormenu,superficie,listatelas)
         self.__contador_menu = 0
+
+    def atualizar(self,ciclo):
+        self.__contador_menu -= 0.3
+        self.fundo = misturacor(psicodelico(self.__contador_menu), [200, 220, 230], 1, 5)
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT: return False
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                acao = self.clicar()
+                if acao == 1:
+                    with open("saves.json","r") as saves:
+                        slots = json.load(saves)
+                    slots[self.__save] = ["Novo Jogo","fase1"]
+                    with open("saves.json","w") as saves:
+                        json.dump(slots,saves)
+                return self.listatelas[acao]
+        return super().atualizar()
+
 
 class Tela_De_Jogo(Tela):
     def __init__(self, superficie, nivel,slot):
